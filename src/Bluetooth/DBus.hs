9add5e0cc04ca568972e29bc8a35d985549f15b1
Julian K. Arni <jkarni@gmail.com>
2016-12-20 12:50:22 -0500

    GetManagerObjects called

--- src/Bluetooth/DBus.hs
+++ src/Bluetooth/DBus.hs
@@ -1,19 +1,46 @@
 module Bluetooth.DBus where
 
 import Bluetooth.Types
+import Control.Monad.Reader
 import qualified Data.Text as T
 import qualified Data.Map as Map
 import DBus
+import DBus.Types (object, DBusArguments(..))
 
+-- | Registers an application (set of services) with Bluez.
 registerApplication :: Application -> BluetoothM ()
-registerApplication app = toBluetoothM $
-  callMethod bluezName bluezPath gattManagerIFace "RegisterApplication" args []
+registerApplication app = do
+  conn <- asks dbusConn
+  addObj <- asks addObject
+  -- The top-level application needs to present an ObjectManager interface
+  liftIO $ addObj (applicationRoot app) appObj
+  toBluetoothM . const
+    $ callMethod bluezName bluezPath gattManagerIFace "RegisterApplication"  args [] conn
+  -- forM_ (applicationServices app) 
   where
     args :: (ObjectPath, Map.Map T.Text Any)
     args = (applicationRoot app, Map.empty)
+
+    appObj :: Object
+    appObj = object "org.freedesktop.DBus.ObjectManager"
+                    objectManagerIFace
+
+    -- See https://dbus.freedesktop.org/doc/dbus-specification.html#standard-interfaces-objectmanager
+    objectManagerIFace :: Interface
+    objectManagerIFace = Interface
+      { interfaceMethods = [getManagedObjects]
+      , interfaceSignals = []
+      , interfaceAnnotations = []
+      , interfaceProperties = []
+      }
+
+    getManagedObjects :: Method
+    getManagedObjects
+      = Method (repMethod (return app :: IO Application))
+               "GetManagedObjects"
+               Done
+               ("rep" :> Done)
  
-connect :: IO DBusConnection
-connect = connectClient System
 
 -- * Constants
 
--- src/Bluetooth/Types.hs
+++ src/Bluetooth/Types.hs
@@ -1,41 +1,65 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Bluetooth.Types where
 
-import qualified Data.Text as T
-import GHC.Generics (Generic)
-import Data.Word (Word16)
-import Data.LargeWord (Word128)
-import Control.Monad.IO.Class (MonadIO)
-import Control.Monad.Except (ExceptT(ExceptT), MonadError, runExceptT)
-import Control.Monad.Reader (ReaderT(ReaderT), runReaderT, MonadReader)
-import DBus (MethodError, DBusConnection, ObjectPath, Representable(..), DBusType(TypeVariant), DBusValue(DBVVariant), SomeDBusValue)
-import DBus.Message (MessageHeader)
-import Numeric (readHex)
-import Data.String (IsString(fromString))
-import Control.Monad.Writer (WriterT, MonadWriter, runWriterT)
-import Control.Monad.Trans (lift)
 
+import Control.Monad.Except (ExceptT(ExceptT), MonadError, runExceptT)
+import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.Reader (ReaderT(ReaderT), runReaderT, MonadReader)
+import DBus (MethodError, DBusConnection, ObjectPath, Representable(..),
+             DBusType(TypeVariant, TypeDict, DBusSimpleType), DBusValue(DBVVariant, DBVDict), connectBus,
+             ConnectionType(System), Object, objectRoot, DBusSimpleType(..), objectPath, objectPathToText)
+import DBus.Types (root)
+import Data.IORef
+import Data.Maybe (fromMaybe)
+import Data.Monoid ((<>))
+import Data.Singletons.TH (genSingletons)
+import Data.String (IsString(fromString))
+import Data.Word (Word16, Word32)
+import GHC.Generics (Generic)
+import Numeric (readHex, showHex)
+
+import qualified Data.UUID as UUID
+import qualified Data.Map as Map
+import qualified Data.Text as T
+
+-- See <http://www.itu.int/rec/T-REC-X.667/en ITU-T Rec. X.677> for more
+-- information on the format and generation of these UUIDs.
 data UUID
-  = OfficialUUID Word16
+  = OfficialUUID16 Word16
+  | OfficialUUID32 Word32
   -- Custom services use 128-bit identifiers
-  | UnofficialUUID Word128
+  | UnofficialUUID UUID.UUID
   deriving (Eq, Show, Generic)
 
 instance IsString UUID where
   fromString x
-    | length x == 32 = UnofficialUUID $ go x
-    | length x == 4  = OfficialUUID $ go x
-    | otherwise      = error "UUID.fromString: expecting 128 or 16-bit UUID"
+    | length x > 8  = UnofficialUUID $ fromMaybe (error "UUID.fromString: invalid UUID") $ UUID.fromString x 
+    | length x == 8 = OfficialUUID32 $ go x
+    | length x == 4 = OfficialUUID16 $ go x
+    | otherwise     = error "UUID.fromString: expecting 16, 32 or 128-bit UUID"
     where
     go y = case [ z | (z, "") <- readHex y ] of
       [val] -> val
       []    -> error "UUID.fromString: no parse"
       _     -> error "UUID.fromString: ambiguous parse"
 
+instance Representable UUID where
+  type RepType UUID = 'DBusSimpleType 'TypeString
+  toRep (UnofficialUUID w) = toRep $ UUID.toText w
+  
+
 data Application = Application
   { applicationRoot :: ObjectPath
   , applicationServices :: [Service]
   } deriving (Eq, Show, Generic)
+
+instance Representable Application where
+  type RepType Application = 'TypeDict 'TypeObjectPath (RepType Service)
+  toRep app = DBVDict $ go <$> applicationServices app
+    where
+      prefixed n = objectPath $ objectPathToText (applicationRoot app) <> "/" <> n
+      go s = (toRep $ prefixed $ serviceName s , toRep s)
       
 data Service = Service
   { serviceName :: T.Text
@@ -43,6 +67,19 @@ data Service = Service
   , serviceCharacteristics :: [Characteristic]
   } deriving (Eq, Show, Generic)
 
+
+
+instance Representable Service where
+  type RepType Service = 'TypeDict 'TypeString 'TypeVariant
+  toRep s = toRep tmap
+    where
+      tmap :: Map.Map T.Text Any
+      tmap = Map.fromList [ ("UUID", MkAny $ serviceUUID s)
+                          -- Only primary services for now
+                          , ("Primary", MkAny $ True)
+                          , ("Characteristics", MkAny ([] :: [ObjectPath]))
+                          ]
+  
 data Characteristic = Characteristic
   { characteristicName :: T.Text
   , characteristicUUID :: UUID
@@ -63,6 +100,7 @@ data Descriptor
   = ExtendedProperties
   | CharacteristicUserDescription
   | ClientCharacteristicConfiguration
+  deriving (Eq, Show, Read, Generic, Ord)
 
 data AdvertisingPacketType
   = ConnectableUndirected
@@ -71,25 +109,32 @@ data AdvertisingPacketType
   | ScannableUndirected
   deriving (Eq, Show, Read, Generic, Ord)
 
-data Handlers = Handlers
-  { methodCallHandler :: MessageHeader -> [SomeDBusValue] -> IO ()
-  , signalHandler     :: MessageHeader -> [SomeDBusValue] -> IO ()
+-- The constructor should not be exported.
+data Connection = Connection
+  { dbusConn :: DBusConnection
+  -- Should it be possible to remove objects?
+  , addObject :: ObjectPath -> Object -> IO ()
   }
 
-instance Monoid Handlers where
-  mempty = let u _ _ = return () in Handlers u u
-  Handlers f1 g1 `mappend` Handlers f2 g2 = Handlers (f1 >> f2) (g1 >> g2) 
+connect :: IO Connection
+connect = do
+  let noHandler _ _ _ = return ()
+  ref <- newIORef mempty
+  let addObj objPath obj = modifyIORef' ref (root objPath obj `mappend`)
+      methodHandler conn hdr val = readIORef ref >>= \f -> objectRoot f conn hdr val
+  dbusC <- connectBus System methodHandler noHandler
+  return $ Connection dbusC addObj
 
 newtype BluetoothM a
-  = BluetoothM ( ReaderT DBusConnection (WriterT Handlers (ExceptT MethodError IO)) a )
+  = BluetoothM ( ReaderT Connection (ExceptT MethodError IO) a )
   deriving (Functor, Applicative, Monad, MonadIO, MonadError MethodError,
-            MonadReader DBusConnection)
+            MonadReader Connection)
 
-runBluetoothM :: BluetoothM a -> DBusConnection -> IO (Either MethodError (a, Handlers))
-runBluetoothM (BluetoothM e) conn = runExceptT $ runWriterT (runReaderT e conn)
+runBluetoothM :: BluetoothM a -> Connection -> IO (Either MethodError a)
+runBluetoothM (BluetoothM e) conn = runExceptT $ runReaderT e conn
 
--- toBluetoothM :: (DBusConnection -> IO (Either MethodError a)) -> BluetoothM a
--- toBluetoothM f =  BluetoothM $ ReaderT . fmap lift
+toBluetoothM :: (Connection -> IO (Either MethodError a)) -> BluetoothM a
+toBluetoothM = BluetoothM . ReaderT . fmap ExceptT
 
 -- | A Haskell existential type corresponding to DBus' @Variant@.
 data Any where
@@ -99,3 +144,5 @@ instance Representable Any where
   type RepType Any = 'TypeVariant
   toRep (MkAny x) = DBVVariant (toRep x)
   fromRep (DBVVariant x) = Just (MkAny x)
+
+-- $(genSingletons [''Characteristic, ''UUID, ''T.Text, ''Service])
