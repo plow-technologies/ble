168b12345a76f7da975f8bfdcabbddb369020d7f
Julian K. Arni <jkarni@gmail.com>
2017-05-08 15:51:41 +0200

    shellcheck fixes

--- test/Mock/start_mock.sh
+++ test/Mock/start_mock.sh
@@ -15,39 +15,39 @@ LOG_TO=${BLE_TEST_LOG_FILE:-/dev/null}
 while getopts l: opt; do
     case $opt in
         l) LOG_TO=$OPTARG ;;
-        \?) printf "Invalid option: $OPTARG" ;;
+        \?) printf "Invalid option: %s" "$OPTARG" ;;
     esac
 done
 
 # echo "Logging to $LOG_TO"
 
 python3 -m dbusmock --system org.bluez.Mock / org.bluez \
-   2>&1 > "$LOG_TO" &
+   > "$LOG_TO" 2>&1 &
 
 sleep 5
 trap 'kill $(jobs -p)' EXIT
 
 gdbus call --system -d org.bluez.Mock -o / \
   -m org.freedesktop.DBus.Mock.AddTemplate 'bluez5' '{}' \
-  2>&1 > "$LOG_TO"
+  > "$LOG_TO" 2>&1
 
 gdbus call --system -d org.bluez.Mock -o / \
   -m org.bluez.Mock.AddAdapter \
   'hci0' \
-  'computer' 2>&1 > "$LOG_TO"
+  'computer' > "$LOG_TO" 2>&1
 
 gdbus call --system -d org.bluez.Mock -o / \
   -m org.bluez.Mock.AddDevice \
   'hci0' \
   '11:22:33:44:55:66' \
-  'Test Device' 2>&1 > "$LOG_TO"
+  'Test Device' > "$LOG_TO" 2>&1
 
 gdbus call --system -d org.bluez.Mock -o / \
   -m org.bluez.Mock.AddGATTService \
   '/org/bluez/hci0' \
   '1' \
   "$SERVICE_UUID" \
-  'true' 2>&1 > "$LOG_TO"
+  'true' > "$LOG_TO" 2>&1
 
 gdbus call --system -d org.bluez.Mock -o / \
   -m org.bluez.Mock.AddGATTCharacteristic \
@@ -56,7 +56,7 @@ gdbus call --system -d org.bluez.Mock -o / \
   "$CHARACTERISTIC_UUID" \
   '["read"]' \
   "$CHARACTERISTIC_VALUE" \
-  'false' 2>&1 > "$LOG_TO"
+  'false' > "$LOG_TO" 2>&1
 
 printf "ready\n"
 
