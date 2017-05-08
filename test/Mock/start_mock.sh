3fde4f5a58fa1d27c90e28a2464f896dbff57727
Julian K. Arni <jkarni@gmail.com>
2017-05-08 15:00:34 +0200

    Logging in travis

--- test/Mock/start_mock.sh
+++ test/Mock/start_mock.sh
@@ -5,37 +5,49 @@ set -o errexit
 # This script registers a mock service with DBus. The service contains one
 # characteristic.
 
+
 SERVICE_UUID=4ea7235c-8d49-4a6f-abe6-1883218a93a7
 CHARACTERISTIC_UUID=6fe4afc7-ebf8-4369-90aa-0fe45064e3f9
 CHARACTERISTIC_VALUE=3
+LOG_TO=${BLE_TEST_LOG_FILE:-/dev/null}
+
+
+while getopts l: opt; do
+    case $opt in
+        l) LOG_TO=$OPTARG ;;
+        \?) printf "Invalid option: $OPTARG" ;;
+    esac
+done
+
+# echo "Logging to $LOG_TO"
 
 python3 -m dbusmock --system org.bluez.Mock / org.bluez \
-   2>&1 > /dev/null &
+   2>&1 > "$LOG_TO" &
 
 sleep 5
 trap 'kill $(jobs -p)' EXIT
 
 gdbus call --system -d org.bluez.Mock -o / \
   -m org.freedesktop.DBus.Mock.AddTemplate 'bluez5' '{}' \
-  2>&1 > /dev/null
+  2>&1 > "$LOG_TO"
 
 gdbus call --system -d org.bluez.Mock -o / \
   -m org.bluez.Mock.AddAdapter \
   'hci0' \
-  'computer' 2>&1 > /dev/null
+  'computer' 2>&1 > "$LOG_TO"
 
 gdbus call --system -d org.bluez.Mock -o / \
   -m org.bluez.Mock.AddDevice \
   'hci0' \
   '11:22:33:44:55:66' \
-  'Test Device' 2>&1 > /dev/null
+  'Test Device' 2>&1 > "$LOG_TO"
 
 gdbus call --system -d org.bluez.Mock -o / \
   -m org.bluez.Mock.AddGATTService \
   '/org/bluez/hci0' \
   '1' \
   "$SERVICE_UUID" \
-  'true' 2>&1 > /dev/null
+  'true' 2>&1 > "$LOG_TO"
 
 gdbus call --system -d org.bluez.Mock -o / \
   -m org.bluez.Mock.AddGATTCharacteristic \
@@ -44,7 +56,7 @@ gdbus call --system -d org.bluez.Mock -o / \
   "$CHARACTERISTIC_UUID" \
   '["read"]' \
   "$CHARACTERISTIC_VALUE" \
-  'false' 2>&1 > /dev/null
+  'false' 2>&1 > "$LOG_TO"
 
 printf "ready\n"
 
