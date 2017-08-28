00dce4bcd104b48c410cc1e27742721827bf0f26
Julian K. Arni <jkarni@gmail.com>
2017-08-28 16:04:54 +0200

    Try using different controllers

--- test/Mock.hs
+++ test/Mock.hs
@@ -1,25 +1,56 @@
 module Mock where
 
-import           Bluetooth                     (UUID)
-import           Bluetooth.Internal.Interfaces (bluezName, bluezPath)
+import           Bluetooth
+import           Bluetooth.Internal.Interfaces (bluezPath)
 import           Data.IORef
-import           Paths_ble
-import           System.IO
-import qualified System.Process                as P
+{-import           Paths_ble-}
+import Control.Monad.IO.Class
+import Control.Concurrent
+import qualified Data.ByteString as BS
+{-import qualified System.Process                as P-}
 
 withAService :: IO a -> IO a
-withAService action = do
-  writeIORef bluezName "org.bluez.Mock"
+withAService action = action `whileRunning` (do
+  app' <- app
+  conn <- connect
+  registered <- runBluetoothM (registerAndAdvertiseApplication app') conn
+  case registered of
+    Right _ -> return ()
+    Left e -> error $ show e)
+
+
+whileRunning :: IO a -> IO () -> IO a
+whileRunning onThisController onThatController = do
+  writeIORef bluezPath "/org/bluez/hci1"
+  thatThread <- forkIO onThatController
+  threadDelay 100000000
   writeIORef bluezPath "/org/bluez/hci0"
-  hSetBuffering stdout LineBuffering
-  hSetBuffering stdin LineBuffering
-  file <- getDataFileName "test/Mock/start_mock.sh"
-  (_inHandle, Just outHandle, _errHandle, proc)
-    <- P.createProcess (P.shell $ "bash " ++ file) { P.std_out = P.CreatePipe }
-  _ <- hGetLine outHandle
-  val <- action
-  P.terminateProcess proc
-  return val
+  result <- onThisController
+  killThread thatThread
+  return result
+
+app :: IO Application
+app = do
+  ref <- newIORef "blah"
+  return $ "/com/turingjump/example/app0"
+    & services .~ [someService ref]
+
+someService :: IORef BS.ByteString -> Service 'Local
+someService ref
+  = "4ea7235c-8d49-4a6f-abe6-1883218a93a7"
+    & characteristics .~ [someCharacteristic ref]
+
+someCharacteristic :: IORef BS.ByteString -> CharacteristicBS 'Local
+someCharacteristic ref
+  = "6fe4afc7-ebf8-4369-90aa-0fe45064e3f9"
+     & readValue ?~ liftIO (readIORef ref)
+     & writeValue ?~ fmap liftIO write
+     & properties .~ [CPNotify, CPWrite, CPRead]
+  where
+    write v = do
+      writeIORef ref v
+      return True
+
 
 mockServiceUUID :: UUID
 mockServiceUUID = "4ea7235c-8d49-4a6f-abe6-1883218a93a7"
